eb36e7b941c560e536774d405120fef669f4b60f
abdessamadelouarrag <abdessamad4804@gmail.com>
2026-01-30 16:27:23 +0100

    update in migration and route

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,32 @@
-<?php
+    <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SearchController;
-use App\Http\Controllers\FriendRequestController;
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    // Route::get('/profile', [UserController::class, 'profile'])->name('profile');
-    Route::post('/upload-image', [UserController::class, 'uploadImage'])->name('upload.image');
-    Route::get('/search', [SearchController::class, 'index'])->name('search');
-    Route::post('/friends/request/{user}', [FriendRequestController::class, 'store'])
-    ->middleware('auth')
-    ->name('friends.request');
+    use App\Http\Controllers\ProfileController;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\UserController;
+    use App\Http\Controllers\SearchController;
+    use App\Http\Controllers\FriendRequestController;
 
 
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-require __DIR__.'/auth.php';
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        // Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+        Route::post('/upload-image', [UserController::class, 'uploadImage'])->name('upload.image');
+        Route::get('/search', [SearchController::class, 'index'])->name('search');
+        Route::post('/friends/request/{user}', [FriendRequestController::class, 'store'])
+        ->middleware('auth')
+        ->name('friends.request');
+
+
+    });
+
+    require __DIR__.'/auth.php';
